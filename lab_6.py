99365a9fd2bdbb399dde9bdd3f278462854a23f2
Unique Orduna <uniqueorduna@swuser-129-8-204-153.swlan.csufresno.edu>
2022-05-11 13:04:06 -0700

    finish

---
+++ lab_6.py
@@ -0,0 +1,15 @@
+"""
+To start, we will generate a random integer between 1 and 20, and 
+based on the result of the random number, we check to see
+if it falls under a certain range
+if the number is greater than > 15, then the result will be "Cherries"
+otherwise if the number is > 10, then the result will be "orange"
+otherwise if the number is > 5, then result will be "plum"
+otherwise if the number is > 2, then the result will be "melon"
+otherwise if the number is > 1, then the result will be "bell"
+if the number is not any of the above, then the result will be "bar"
+
+we iterate over using a loop three times and print the results to the user.
+as an example "plum cherries"
+
+"""
